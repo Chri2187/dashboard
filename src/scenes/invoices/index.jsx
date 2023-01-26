ee981b0a78218948da03bc3ed739ab4f3f5638de
Christian Francini Mascagni <christian.francini87@gmail.com>
2023-01-26 17:45:18 +0100

    minor update

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,16 +1,17 @@
 import { Box, Typography, useTheme } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { tokens } from '../../theme';
-import { mockDataTeam } from '../../data/mockData';
-import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
-import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
-import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import { mockDataInvoices } from '../../data/mockData';
 import Header from '../../components/Header';
-const Team = () => {
+const Invoices = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const columns = [
-        { field: 'id', headerName: 'ID' },
+        { field: 'id', headerName: 'ID', flex: 0.5 },
+        {
+            field: 'registrarId',
+            headerName: 'Registrar ID',
+        },
         {
             field: 'name',
             headerName: 'Name',
@@ -31,41 +32,25 @@ const Team = () => {
         },
         { field: 'email', headerName: 'Email', flex: 1 },
         {
-            field: 'access',
-            headerName: 'Access Level',
+            field: 'address',
+            headerName: 'Address',
             flex: 1,
-            renderCell: ({ row: { access } }) => {
-                return (
-                    <Box
-                        width='60%'
-                        m='0 auto'
-                        p='5px'
-                        display='flex'
-                        justifyContent='center'
-                        backgroundColor={
-                            access === 'admin'
-                                ? colors.greenAccent[600]
-                                : colors.greenAccent[700]
-                        }
-                        borderRadius='4px'
-                    >
-                        {access === 'admin' && (
-                            <AdminPanelSettingsOutlinedIcon />
-                        )}
-                        {access === 'manager' && <SecurityOutlinedIcon />}
-                        {access === 'user' && <LockOpenOutlinedIcon />}
-                        <Typography color={colors.grey[100]} sx={{ ml: '5px' }}>
-                            {access}
-                        </Typography>
-                    </Box>
-                );
-            },
+        },
+        {
+            field: 'city',
+            headerName: 'City',
+            flex: 1,
+        },
+        {
+            field: 'zipCode',
+            headerName: 'ZIP Code',
+            flex: 1,
         },
     ];
 
     return (
         <Box m='20px'>
-            <Header title='Team' subtitle='Managing the team members' />
+            <Header title='Contacts' subtitle='List of Contacts' />
             <Box
                 m='40px 0 0 0'
                 height='75vh'
@@ -90,12 +75,19 @@ const Team = () => {
                         backgroundColor: colors.blueAccent[700],
                         borderTop: 'none',
                     },
+                    '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+                        color: `${colors.grey[100]} !important`,
+                    },
                 }}
             >
-                <DataGrid rows={mockDataTeam} columns={columns} />
+                <DataGrid
+                    rows={mockDataInvoices}
+                    columns={columns}
+                    components={{ Toolbar: GridToolbar }}
+                />
             </Box>
         </Box>
     );
 };
 
-export default Team;
+export default Invoices;
